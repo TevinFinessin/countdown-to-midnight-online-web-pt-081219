f8dd63799985ee8091e5aa1d98a0d56e4551ea5a
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-17 14:38:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
+ def countdown(num) 
+ while num > 0 
+    puts "#{num} SECOND(S)!"
+     num -= 1 
+   end 
+   return "HAPPY NEW YEAR!"
+ end 
+#x = 1
+#while x < 10
+ # puts "#{x} is less than 10"
+#  x += 1
+#end
 
-def countdown
-end
+#while num_of_hotdogs_eaten < 7
+  #num_of_hotdogs_eaten += 1
+  #puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
+#end
