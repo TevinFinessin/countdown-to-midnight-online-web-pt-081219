119b0a4556448500064ff81f6f4f3b60a2676c29
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-17 14:40:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,13 +6,5 @@
    end 
    return "HAPPY NEW YEAR!"
  end 
-#x = 1
-#while x < 10
- # puts "#{x} is less than 10"
-#  x += 1
-#end
 
-#while num_of_hotdogs_eaten < 7
-  #num_of_hotdogs_eaten += 1
-  #puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
-#end
+def countdown_with_sleep
